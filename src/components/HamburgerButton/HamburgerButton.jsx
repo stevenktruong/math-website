c0a9ca20154dd11ee55bb164f10085d6c799482a
stevenktruong <steventruong@g.ucla.edu>
2020-08-08 15:13:04 -0700

    Created a hamburger menu button on mobile

---
+++ src/components/HamburgerButton/HamburgerButton.jsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+
+import styles from "./HamburgerButton.module.scss";
+
+export default class HamburgerButton extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const value = props.value;
+        this.state = {
+            active: value ? value : false,
+        };
+    }
+
+    updateState() {
+        this.setState({ active: this.props.value });
+    }
+
+    render() {
+        return (
+            <div
+                className={styles.HamburgerButton}
+                onClick={async () => {
+                    await this.props.onClick();
+                    this.updateState();
+                }}
+            >
+                <div
+                    className={this.state.active ? `${styles.TopBar} ${styles.TopBarActive}` : `${styles.TopBar}`}
+                ></div>
+                <div
+                    className={
+                        this.state.active ? `${styles.MiddleBar} ${styles.MiddleBarActive}` : `${styles.MiddleBar}`
+                    }
+                ></div>
+                <div
+                    className={
+                        this.state.active ? `${styles.BottomBar} ${styles.BottomBarActive}` : `${styles.BottomBar}`
+                    }
+                ></div>
+            </div>
+        );
+    }
+}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import HamburgerButton from "components/HamburgerButton";
 import styles from "./Navbar.module.scss";
 
 const navLinks = {
@@ -13,18 +14,43 @@ const navLinks = {
 };
 
 export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mobileMenuActive: false,
+        };
+    }
+
+    toggleMobileMenu() {
+        const newState = !this.state.mobileMenuActive;
+        this.setState({ mobileMenuActive: newState });
+    }
+
+    renderListOfButtons(isMobile = false) {
+        return (
+            <ul className={isMobile ? styles.ShowButtons : null}>
+                {Object.keys(navLinks).map((key, i) => (
+                    <li key={key} className={i === Object.keys(navLinks).length - 1 ? "clearfix" : null}>
+                        <Link href={navLinks[key].href}>
+                            <a onClick={() => this.toggleMobileMenu()}>{navLinks[key].title}</a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+
     render() {
+        const mobileMenuActive = this.state.mobileMenuActive;
         return (
             <nav className={styles.Navbar}>
-                <ul>
-                    {Object.keys(navLinks).map((key, i) => (
-                        <li key={key} className={i === Object.keys(navLinks).length - 1 ? "clearfix" : null}>
-                            <Link href={navLinks[key].href}>
-                                <a>{navLinks[key].title}</a>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+                <div className={styles.Desktop}>{this.renderListOfButtons()}</div>
+                <div className={styles.Mobile}>
+                    <div className={styles.NavbarBar}></div>
+                    <HamburgerButton value={mobileMenuActive} onClick={() => this.toggleMobileMenu()} />
+                    {this.renderListOfButtons(mobileMenuActive)}
+                </div>
             </nav>
         );
     }
