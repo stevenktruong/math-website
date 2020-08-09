01170422df3fea48321dc5c4cd3adb8d212acfd2
stevenktruong <steventruong@g.ucla.edu>
2020-08-08 17:37:29 -0700

    First attempt at CI/CD

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# Ensure we don't push in-progress files
-git stash
-git checkout master
-
-# Cleanup and build
-rm -rf .next build
-yarn build:production
-
-# Upload build
-echo ""
-echo "Uploading build..."
-rsync -r build/ math:public_html
+rsync -r build $USER@$HOST:public_html
 ssh math "chmod -R u+rwX,go+rX,go-w public_html"
 
 echo "Done!"
