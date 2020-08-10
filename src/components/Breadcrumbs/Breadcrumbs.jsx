23869f947ae51b0d8886698d20d3c06ae06ef76b
stevenktruong <steventruong@g.ucla.edu>
2020-08-10 01:37:54 -0700

    Account for ~steven on live website

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,7 +1,12 @@
 import * as React from "react";
+import getConfig from "next/config";
 import { withRouter } from "next/router";
 
 import styles from "./Breadcrumbs.module.scss";
+
+const { publicRuntimeConfig = {} } = getConfig() || {};
+
+const separator = ">";
 
 class Breadcrumbs extends React.Component {
     constructor(props) {
@@ -10,21 +15,25 @@ class Breadcrumbs extends React.Component {
     }
 
     render() {
-        const currentPath = this.state.router.asPath.split("/").slice(1);
+        const fullPath = this.state.router.asPath.split("/");
+        const currentPathBeginning = fullPath.indexOf(`${publicRuntimeConfig.staticFolder}`) + 1;
+        const currentPath = fullPath.slice(currentPathBeginning);
         return (
             <div className={styles.Breadcrumbs}>
-                <a className={styles.Crumb} href="/" key="Home">
+                <a className={styles.Crumb} href={`${publicRuntimeConfig.staticFolder}/`} key="Home">
                     home
                 </a>
                 {currentPath.map((level, index) => {
                     const isLast = index === currentPath.length - 1;
                     return (
                         <>
-                            {" > "}
+                            {` ${separator} `}
                             {!isLast ? (
                                 <>
                                     <a
-                                        href={`/${currentPath.slice(0, index + 1).join("/")}`}
+                                        href={`${publicRuntimeConfig.staticFolder}/${currentPath
+                                            .slice(0, index + 1)
+                                            .join("/")}`}
                                         className={styles.Crumb}
                                         key={level}
                                     >
