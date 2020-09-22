34f23fd0562d7c762055e5f6d3e4220f962bfa99
stevenktruong <steventruong@g.ucla.edu>
2020-09-22 01:56:11 -0700

    Second attempt at LinkedIn

--- src/config/linkedinMeta.jsx
+++ src/config/linkedinMeta.jsx
@@ -2,6 +2,8 @@ import { publicRuntimeConfig } from "helpers";
 
 export const linkedinMeta = (
     <>
-        <meta property="og:image" content={`${publicRuntimeConfig.staticFolder}/images/linkedin-preview.webp`} />
+        <meta name="og:title" content="Teaching" />
+        <meta name="og:description" content="A list of all classes I'm TAing or have TA'd for at UCLA." />
+        <meta name="og:image" content={`${publicRuntimeConfig.staticFolder}/images/linkedin-preview.webp`} />
     </>
 );
