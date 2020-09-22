4cfefde896bcbb9ff04720fda5b2c61f093e7d11
stevenktruong <steventruong@g.ucla.edu>
2020-09-22 02:02:26 -0700

    Use external link for LinkedIn image

--- src/config/linkedinMeta.jsx
+++ src/config/linkedinMeta.jsx
@@ -4,6 +4,9 @@ export const linkedinMeta = (
     <>
         <meta name="og:title" content="Teaching" />
         <meta name="og:description" content="A list of all classes I'm TAing or have TA'd for at UCLA." />
-        <meta name="og:image" content={`${publicRuntimeConfig.staticFolder}/images/linkedin-preview.webp`} />
+        <meta
+            name="og:image"
+            content="https://en.ccunesco.ca/-/media/Images/Unesco/Blog/2020/03/International-Day-of-Math.png?h=600&la=en&w=1400&hash=8477E7F958B5DEBE1200485DC130EA68A4878B0E"
+        />
     </>
 );
