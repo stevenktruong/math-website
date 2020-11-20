1a66c2cc8c7e5c007e387b6743979e977343449e
stevenktruong <steventruong@g.ucla.edu>
2020-11-19 21:04:40 -0800

    Hopefully fix relative linking

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const basePath = process.env.URL_ENV === "production" ? "/~steven" : "";
 
 module.exports = {
     basePath: basePath,
+    trailingSlash: true,
     exportTrailingSlash: true,
     publicRuntimeConfig: {
         staticFolder: basePath,
