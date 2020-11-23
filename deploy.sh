3dad5fcbbe62c181b6f08de1ba2fcadd7da816ac
stevenktruong <steventruong@g.ucla.edu>
2020-11-22 21:18:11 -0800

    Removed Travis configuration and made deploy.sh more robust

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 SSH_TARGET="$USER@$HOST"
 
-rsync -r --delete build/ $SSH_TARGET:public_html/
-ssh $SSH_TARGET "chmod -R u+rwX,go+rX,go-w public_html"
+rsync -r --delete build/ $SSH_TARGET:public_html/ || { echo "Could not run rsync"; exit 1 }
+ssh $SSH_TARGET "chmod -R u+rwX,go+rX,go-w public_html"  || { echo "Could not make website public"; exit 1 }
 
 echo "Done!"
+exit 0
