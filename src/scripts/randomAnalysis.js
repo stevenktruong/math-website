7fc2b9b1edbcb63551c2ae5a5406f6a9e724f710
stevenktruong <steventruong@g.ucla.edu>
2021-02-06 14:15:13 -0800

    Updated scripts to avoid using import statements

--- src/scripts/randomAnalysis.js
+++ src/scripts/randomAnalysis.js
@@ -1,7 +1,11 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
-import { dataDirectory, readDirectoryContents } from "./utils.js";
+const dataDirectory = path.join(process.cwd(), "data");
+
+const readDirectoryContents = folderPath => {
+    return fs.existsSync(folderPath) ? fs.readdirSync(folderPath) : [];
+};
 
 const randomInt = max => {
     return Math.floor(Math.random() * max);
@@ -28,12 +32,10 @@ const quarterMap = {
     f: "Fall",
 };
 
-export const randomProblem = () => {
-    const { qual, problem } = getRandomProblem();
+const { qual, problem } = getRandomProblem();
 
-    const match = qual.match(/([0-9]+)([fs])/);
-    const year = match[1];
-    const quarter = quarterMap[match[2]];
+const match = qual.match(/([0-9]+)([fs])/);
+const year = match[1];
+const quarter = quarterMap[match[2]];
 
-    console.log(`${quarter} 20${year} - Problem ${problem}`);
-};
+console.log(`${quarter} 20${year} - Problem ${problem}`);
