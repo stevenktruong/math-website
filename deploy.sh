432d7d4c9e8930e5bfb0bc6242637e503f270ce5
stevenktruong <steventruong@g.ucla.edu>
2024-06-15 18:10:34 -0700

    Updated yarn scripts and attempt to connect to VPN in GitHub action

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 SSH_TARGET="$USER@$HOST"
 
+echo "Deploying to $SSH_TARGET"
+
 rsync -r --delete build/ $SSH_TARGET:public_html/ || { echo "Could not run rsync" && exit 1; }
 ssh $SSH_TARGET "chmod -R u+rwX,go+rX,go-w public_html" || { echo "Could not make website public" && exit 1; }
 
